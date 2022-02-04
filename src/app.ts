e12331fdaaf21b6696485208d9966839cd558676
seungdonchoi <dchoifor2@gmail.com>
2022-02-04 03:15:56 -0800

    added css and input boxes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
 // Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input'
+    this.attach();
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
